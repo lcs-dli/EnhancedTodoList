3c9bf3cb882cad912d0fa0bb824a6180397b67b8
David Li <dtli@lcs.on.ca>
2024-02-03 20:50:12 -0500

    Added a view to tell the user what to do when the app first launches without any data existing yet.

--- EnhancedTodoList/Views/TodoListView.swift
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -9,14 +9,27 @@ import SwiftUI
 
 struct TodoListView: View {
     // MARK: Stored properties
-        
-        // Our list of items to complete
-        @State private var items: [TodoItem] = []
-        
-        // MARK: Computed properties
-        var body: some View {
-            NavigationStack {
-                VStack {
+    
+    // Our list of items to complete
+    @State private var items: [TodoItem] = []
+    
+    // MARK: Computed properties
+    var body: some View {
+        NavigationStack {
+            VStack {
+                if items.isEmpty {
+                    
+                    ContentUnavailableView(label: {
+                        Label(
+                            "Nothing to do",
+                            systemImage: "powersleep"
+                        )
+                        .foregroundStyle(.green)
+                    }, description: {
+                        Text("To-do items will appear here once you add some.")
+                    })
+                    
+                } else {
                     
                     List(items) { currentItem in
                         Label {
@@ -25,18 +38,18 @@ struct TodoListView: View {
                             Image(systemName: currentItem.isCompleted ? "checkmark.circle" : "circle")
                         }
                     }
+                    
                 }
-                .navigationTitle("Tasks")
             }
             .onAppear {
                 // Populate with example data
                 if items.isEmpty {
-                    items.append(contentsOf: exampleData)
+                    // items.append(contentsOf: exampleData)
                 }
             }
         }
+    }
 }
-
 #Preview {
     TodoListView()
 }
