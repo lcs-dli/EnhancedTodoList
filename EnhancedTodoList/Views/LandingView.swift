a48118569ea7df7fe542b1d45fae2ee773915301
David Li <dtli@lcs.on.ca>
2024-02-03 20:42:25 -0500

    Initial project organization

---
+++ EnhancedTodoList/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedTodoList
+//
+//  Created by David Li on 2024-02-03.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
