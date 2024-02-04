dc663fbcae6464026624345df6b92a39822ab1cc
David Li <dtli@lcs.on.ca>
2024-02-03 20:44:51 -0500

    Add tab View

---
+++ EnhancedTodoList/Views/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  EnhancedTodoList
+//
+//  Created by David Li on 2024-02-03.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text("Statistics View")
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
---
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  EnhancedTodoList
+//
+//  Created by David Li on 2024-02-03.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text("To-do list view")
+    }
+}
+
+#Preview {
+    TodoListView()
+}
